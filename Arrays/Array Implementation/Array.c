407b2e4e68130e62f3f168a5f243524418ff986a
bendazoo <abendezu385@gmail.com>
2024-05-24 04:02:53 -0400

    Added more methods to array implementation

--- Arrays/Array Implementation/Array.c
+++ Arrays/Array Implementation/Array.c
@@ -3,8 +3,17 @@
 int size = 0;
 int count = 0;
 
+void countArr(){
+    printf("The total number of elements in the array is: %d\n", count);
+    printf("\n-----------------------------\n");
+}
+
 void insertElement(int arr[], int index, int value){
-    *(arr +index) = value;
+
+
+    if(index >= size) size*= 2;
+
+    *(arr + index) = value;
     count++;
 }
 
@@ -17,11 +26,23 @@ void removeElement(int arr[], int value,int size){
     }
 }
 
-int readValue(const int arr[], int index){
+void readValue(const int arr[], int index){
 
     int value = *(arr+index);
 
-    return value;
+    printf("The value at the index is: %d\n", value);
+    printf("\n-----------------------------\n");
+}
+
+void readArray(int arr[], int size){
+    printf("The array is: [ ");
+    for(int i = 0; i< size; i++){
+        printf("%d ", *(arr+i));
+    }
+    printf("]");
+    printf("\n-----------------------------\n");
+
+
 }
 
 int main() {
@@ -32,24 +53,27 @@ int main() {
     int arr[size];
     int *pArr = arr;
 
-    insertElement(pArr, 3, 10);
-    insertElement(pArr, 5, 40);
-    insertElement(pArr, 6, 212);
-    insertElement(pArr, 2, 432);
-
-    printf("The value at the index is: %d\n", readValue(pArr, 3));
-
-    for(int i =0; i<size;i++){
-
-        printf("%d\n", *(pArr+i));
-    }
-    printf("---------------\n");
+    insertElement(pArr, 0, 10);
+    insertElement(pArr, 1, 40);
+    insertElement(pArr, 2, 212);
+    insertElement(pArr, 3, 432);
+    insertElement(pArr, 4, 102);
+    countArr();
+    readValue(pArr,3);
+    readArray(pArr, size);
     removeElement(pArr, 10, size);
+    readArray(pArr, size);
 
-    for(int i =0; i<size;i++){
+    insertElement(pArr, 8, 120);
+    insertElement(pArr, 9, 1024);
+    insertElement(pArr, 7, 1220);
+    insertElement(pArr, 6, 3120);
+    insertElement(pArr, 5, 5120);
 
-        printf("%d\n", *(pArr+i));
-    }
+    countArr();
+    readArray(pArr, size);
+
+
 
     return 0;
 }
